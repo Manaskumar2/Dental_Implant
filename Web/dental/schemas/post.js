a154dbd320608814c82c783c79c3d230251e058b
Rafeeq <rsyed0904@gmail.com>
2023-05-10 14:15:02 +0530

    Changed sanity post  to service

--- Web/dental/schemas/post.js
+++ Web/dental/schemas/post.js
@@ -2,7 +2,7 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'post',
-  title: 'Post',
+  title: 'Services',
   type: 'document',
   fields: [
     defineField({
